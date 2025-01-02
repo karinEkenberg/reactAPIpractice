3647894401666fdb6672e047df6dd4726c20d57c
Karin Ekenberg <137770952+karinEkenberg@users.noreply.github.com>
2025-01-02 15:06:44 +0100

    first commit

---
+++ api/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Api from "./components/Api";
+
+function App() {
+  return (
+    <>
+      <Api></Api>
+    </>
+  );
+}
+
+export default App;
---
+++ api/src/components/Api.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function Api() {
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState({});
+
+  interface Todo {
+    title: string;
+    completed: boolean;
+    id: number;
+  }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((res) => setTodos(res.slice(0, 10)))
+      .catch((error) => setError(error));
+  }, []);
+  return (
+    <div>
+      <h1>API Data</h1>
+      {Object.keys(error).length > 0 ? (
+        <p style={{ color: "red" }}>Error: {error.message}</p>
+      ) : (
+        <div>
+          {todos.length > 0 ? (
+            <ul>
+              {todos.map((todo: Todo) => (
+                <li key={todo.id}>{todo.title}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
